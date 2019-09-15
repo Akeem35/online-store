d52f11e4a2ad83e45f1bd9704a07ca9e1a27487d
Akeem <raulakeem35@gmail.com>
2019-09-14 20:58:18 -0500

    First commit

---
+++ pages/About/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>This is the about Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const res = await $axios.$get('http://localhost:5000/api/products/')
+    console.log(res)
+  }
+}
+</script>
+
+<style>
+
+</style>
